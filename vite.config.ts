80713753c1c73ced837107abaf18d7cc2740809c
LiuYang <liuyang@vchangyi.com>
2023-02-20 03:13:18 +0000

    base config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
 /**
  * details about vite-plugin-pages
  * https://www.npmjs.com/package/vite-plugin-pages?activeTab=readme
  * https://stackoverflow.com/questions/71906886/how-to-setup-vite-plugin-pages-cannot-find-module-pages
  * https://nextjs.org/docs/routing/introduction
  */
-import Pages from 'vite-plugin-pages'
+import Pages from 'vite-plugin-pages';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -26,4 +26,4 @@ export default defineConfig({
       '@': `${path.resolve(__dirname, './src')}`
     }
   }
-})
+});
